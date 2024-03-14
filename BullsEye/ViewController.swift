d3bbae8426fbdaa000ee6ebcc54fb7e963f7b528
Thomas Neylon <thomasfneylon@gmail.com>
2024-03-14 17:19:01 -0400

    added flare
    game runs, its is now prettier,
    about button still needs functionality

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -19,9 +19,39 @@ class ViewController: UIViewController {
     var score = 0
     var round = 0
     
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let thumbImageNormal = UIImage(named: "SliderThumb-Normal")!
+        slider.setThumbImage(thumbImageNormal, for: .normal)
+        
+        let thumbImageHighlighted = UIImage(named: "SliderThumb-Highlighted")!
+        slider.setThumbImage(thumbImageHighlighted, for: .highlighted)
+        
+        let insets = UIEdgeInsets(
+            top: 0,
+            left: 14,
+            bottom: 0,
+            right: 14)
+        
+        let trackLeftImage = UIImage(named: "SliderTrackLeft")!
+        let trackLeftResizable = trackLeftImage.resizableImage(withCapInsets: insets)
+        slider.setMinimumTrackImage(trackLeftResizable, for: .normal)
+        
+        let trackRightImage = UIImage(named: "SliderTrackRight")!
+        let trackRightResizable = trackRightImage.resizableImage(withCapInsets: insets)
+        slider.setMaximumTrackImage(trackRightResizable, for: .normal)
+        
         // Do any additional setup after loading the view.
+        startNewGame()
+    }
+    
+    @IBAction func startNewGame() {
+        score = 0
+        round = 0
         startNewRound()
     }
 
@@ -33,27 +63,46 @@ class ViewController: UIViewController {
             //could also do let difference = abs(targetValue - currentValue)  abs gives you a number without reguard to its sign, its absolut value, so it is always positive, which is also why difference is defined with let, its value wont change unlike the above examples
         }
         
-        let points = 100 - difference
-        score += points
+        var points = 100 - difference
         
+        let title: String
+        if difference == 0 {
+            title = "PERFECT!"
+            points += 100
+        } else if difference < 5 {
+            title = "You almost had it!"
+            if difference == 1 {
+                points += 50
+            }
+        } else if difference < 10 {
+            title = "Prett good!"
+        } else if difference < 20 {
+            title = "You need some practice"
+        } else {
+            title = "Not even close..."
+        }
+        
+        score += points
         
         let message = "You Scored \(points) points!"
         
         let alert = UIAlertController(
-            title: "Hello, World",
+            title: title,
             message: message,
             preferredStyle: .alert)
         
         let action = UIAlertAction(
             title: "OK",
             style: .default,
-            handler: nil)
+            handler: { _ in
+                self.startNewRound()
+            })
         
         alert.addAction(action)
         
         present(alert, animated: true, completion: nil)
         
-        startNewRound()
+        
     }
     
     @IBAction func sliderMoved(_ slider: UISlider) {
@@ -75,6 +124,8 @@ class ViewController: UIViewController {
         scoreLabel.text = String(score)
         roundLabel.text = String(round)
     }
+    
+    
 
 }
 
